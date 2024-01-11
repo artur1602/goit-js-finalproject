5cee31d79ff518714c2f06f6c7e7dc61b4ba7281
Stanishovskyi <arturstepanovi4@gmail.com>
2024-01-11 13:36:01 +0200

    clone hw

---
+++ js/01-gallery.js
@@ -0,0 +1,66 @@
+import { galleryItems } from './gallery-items.js';
+
+// Change code below this line
+
+const instance = basicLightbox.create('', {
+  onShow: () => {
+    document.onkeydown = event => {
+      if (event.key === 'Escape') {
+        instance.close();
+      }
+    };
+  },
+  onClose: () => {
+    document.onkeydown = undefined;
+  }
+});
+
+const modalElement = instance.element();
+
+const galleryRoot = document.querySelector('.gallery');
+
+function createGalleryItem(item) {
+  return `<li class="gallery__item">
+            <a class="gallery__link" href="${item.original}">
+             <img class="gallery__image" src="${item.preview}" alt="${item.description}" data-source="${item.original}" />
+           </a>
+          </li>`;
+}
+
+function createGallery() {
+  const galleryHtmlElements = galleryItems.map(item => createGalleryItem(item)).join('');
+
+  if (!galleryRoot) {
+    return;
+  }
+
+  galleryRoot.insertAdjacentHTML('beforeend', galleryHtmlElements);
+}
+
+function createModalImage(url) {
+  return `<img src="${url}" width="800" height="600"/>`;
+}
+
+function bindEvents() {
+  galleryRoot.querySelectorAll('.gallery__link').forEach(item => {
+    item.addEventListener('click', event => {
+      event.preventDefault();
+
+      const imageUrl = event.currentTarget.querySelector('img')?.dataset.source;
+
+      if (!imageUrl) {
+        return;
+      }
+
+      modalElement.innerHTML = createModalImage(imageUrl);
+
+      instance.show();
+    });
+  });
+
+  modalElement.addEventListener('click', () => instance.close());
+}
+
+createGallery();
+
+bindEvents();
